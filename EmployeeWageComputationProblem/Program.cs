470f43181984d86c89ad457090ed8c8bd936cc6d
Prateek Pai <paiprateek123@gmail.com>
2022-06-10 07:44:17 +0530

    Calculate Daily Employee Wage (including Part time Work)

--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -13,11 +13,12 @@ namespace EmployeeWageComputationProblem
         {
             int WagePerHour = 20;
             int FullDayHour = 8;
+            int PartTimeHour = 4;
             int EmployeeWage=0;
             int Absent=0;
             int Present = 1;
             Random random = new Random();
-            int Presence = random.Next(0,2);
+            int Presence = random.Next(0,3);
 
             if(Presence == 0)
             {
@@ -26,12 +27,18 @@ namespace EmployeeWageComputationProblem
 
 
             }
-            else
+            else if (Presence==1)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present (Full Time)");
                 EmployeeWage = (WagePerHour * Present) * FullDayHour;
 
             }
+            else
+            {
+                Console.WriteLine("Employee is Present (Part Time)");
+                EmployeeWage = (WagePerHour * Present) * PartTimeHour;
+            }
+
             Console.WriteLine("Employee Daily Wage is :{0}", EmployeeWage);
         }
     }
