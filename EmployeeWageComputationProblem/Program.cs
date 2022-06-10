dcd1cc32092c061f89c426c0fad992cdad40cc8f
Prateek Pai <paiprateek123@gmail.com>
2022-06-10 07:38:39 +0530

    Calculate Daily Employee Wage

--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -11,17 +11,28 @@ namespace EmployeeWageComputationProblem
         }
         public static void CheckEmployeePresence()
         {
+            int WagePerHour = 20;
+            int FullDayHour = 8;
+            int EmployeeWage=0;
+            int Absent=0;
+            int Present = 1;
             Random random = new Random();
             int Presence = random.Next(0,2);
 
             if(Presence == 0)
             {
                 Console.WriteLine("Employee is Absent");
+                EmployeeWage = (WagePerHour * Absent)*FullDayHour;
+
+
             }
             else
             {
                 Console.WriteLine("Employee is Present");
+                EmployeeWage = (WagePerHour * Present) * FullDayHour;
+
             }
+            Console.WriteLine("Employee Daily Wage is :{0}", EmployeeWage);
         }
     }
 }
