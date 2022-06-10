1af8916a5eab7ff7b7e85a314481ec398076ac2c
Prateek Pai <paiprateek123@gmail.com>
2022-06-10 09:22:55 +0530

    Calculate Wages till 100 Working Hours and 20 Working Days

--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -20,10 +20,15 @@ namespace EmployeeWageComputationProblem
             int AbsentPresent=0;
             int Absent=0;
             int Present = 1;
-            int WorkingDaysPerMonth = 20;
             int TotalWagePerMonth=0;
+            int MaxWorkingHour = 100;
+            int MaxWorkingDays = 20;
+            int TotalWorkingHour = 0;
+            int TotalWorkingDays = 0;
+            int WorkingHour = 0;
+            int WorkingDays = 0;
 
-            for(int day=0;day<WorkingDaysPerMonth;day++)
+            while(TotalWorkingDays < MaxWorkingDays && TotalWorkingHour < MaxWorkingHour)
             {
 
                 Random random = new Random();
@@ -34,29 +39,39 @@ namespace EmployeeWageComputationProblem
                     case 0:
                         Console.WriteLine("Employee is Absent");
                         EmployeeWage = (WagePerHour * Absent);
+                        WorkingHour = Absent;
                         AbsentPresent++;
                         break;
 
                     case 1:
                         Console.WriteLine("Employee is Present (Full Time)");
                         EmployeeWage = (WagePerHour * Present) * FullDayHour;
+                        WorkingHour = FullDayHour;
                         FullDayPresent++;
                         break;
 
                     case 2:
                         Console.WriteLine("Employee is Present (Part Time)");
                         EmployeeWage = (WagePerHour * Present) * PartTimeHour;
+                        WorkingHour = PartTimeHour;
                         PartTimePresent++;
                         break;
                 }
+                TotalWorkingHour+=WorkingHour;
                 TotalWagePerMonth += EmployeeWage;
-                Console.WriteLine("Employee Day{0} Wage is :{1}",day,EmployeeWage);
+                Console.WriteLine("Employee Daily Wage is :{0}",EmployeeWage);
                 Console.WriteLine("..........................................");
             }
             Console.WriteLine("Employee Monthly Wage is :{0}", TotalWagePerMonth);
+            Console.WriteLine("..........................................");
             Console.WriteLine("Number of Day Employee Absent in a Month is: " + AbsentPresent);
+            Console.WriteLine("..........................................");
             Console.WriteLine("Number of Day Employee Present (Full Day) in a Month is: " + FullDayPresent);
+            Console.WriteLine("..........................................");
             Console.WriteLine("Number of Day Employee Present (Part Time) in a Month is: " + PartTimePresent);
+            Console.WriteLine("..........................................");
+            Console.WriteLine("Total Working Hours is :" + TotalWorkingHour);
+            Console.WriteLine("..........................................");
         }
     }
 }
